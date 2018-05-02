4dba4158734af4b9a1728cfc030b40ab1c6ec209
renshihan <379595488@qq.com>
2018-05-02 13:47:13 +0800

    修改调用错误

--- sofa-boot-study-api/src/main/java/com/renshihan/sofa/api/service/HelloSyncService.java
+++ sofa-boot-study-api/src/main/java/com/renshihan/sofa/api/service/HelloSyncService.java
@@ -1,9 +1,6 @@
 package com.renshihan.sofa.api.service;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 
 /**
  * @author renshihan@winchannel.net
@@ -15,5 +12,5 @@ import javax.ws.rs.Produces;
 public interface HelloSyncService {
     @GET
     @Path("/restService/{id}")
-    String saySync(String id);
+    String saySync(@PathParam("id") String id);
 }
--- sofa-boot-study-client/src/main/java/com/renshihan/sofa/sofabootstudy/client/controller/HelloClientController.java
+++ sofa-boot-study-client/src/main/java/com/renshihan/sofa/sofabootstudy/client/controller/HelloClientController.java
@@ -5,21 +5,20 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author renshihan@winchannel.net
  * @date 2018/4/27 17:51
  */
-@Controller
+@RestController
 @Slf4j
 public class HelloClientController {
     @Autowired
     private HelloSyncService helloSyncService;
     @RequestMapping("/hello")
-    public void hello(){
+    public String hello(){
         log.info("[客户端]进入....");
-        log.info("[客户端]调用服务"+helloSyncService.saySync("你好"));
-        log.info("[客户端]结束....");
-
+        return "[客户端]调用服务:"+helloSyncService.saySync("你好");
     }
 }
